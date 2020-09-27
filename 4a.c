a434feb157e8210b30f4b108229d1dde3b9c31ab
Rushikesh <rushikesh0203@gmail.com>
2020-09-28 03:16:55 +0530

    4

---
+++ 4a.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+extern int errno;
+int main(){
+
+int fd = open("foobar.txt",O_RDWR | O_CREAT);
+
+if (fd==-1)
+{
+	perror("Error occured during open call\n");
+	printf("%d\n",errno);
+	return 0;
+}
+
+printf("fd: %d \n",fd);
+getchar();
+
+
+
+return 0;	
+}
+
---
+++ 4b.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+
+int main(){
+
+int fd = open("foo.txt",O_RDWR | O_CREAT | O_EXCL , S_IRWXU| S_IRGRP | S_IROTH);
+
+if (fd==-1)
+{
+	perror("Error occured during open call");
+	return 0;
+}
+
+printf("%d",fd);
+getchar();
+
+
+
+return 0;	
+}
+
