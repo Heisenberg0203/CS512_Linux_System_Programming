53965e4bd9e1ed0f9c49dd22e891d4ad8f102528
Rushikesh <rushikesh0203@gmail.com>
2020-09-28 03:16:32 +0530

    22

---
+++ 2.c
@@ -0,0 +1,14 @@
+int main(){
+
+for(;;);
+return 0;
+}
+/*
+rushikesh@rushikesh:~/Desktop/lsp/handsonlist1$ ls /proc/5314/
+arch_status  cmdline          exe      loginuid   mountstats  oom_score_adj  sched         stack    timers
+attr         comm             fd       map_files  net         pagemap        schedstat     stat     timerslack_ns
+autogroup    coredump_filter  fdinfo   maps       ns          patch_state    sessionid     statm    uid_map
+auxv         cpuset           gid_map  mem        numa_maps   personality    setgroups     status   wchan
+cgroup       cwd              io       mountinfo  oom_adj     projid_map     smaps         syscall
+clear_refs   environ          limits   mounts     oom_score   root           smaps_rollup  task
+*/
