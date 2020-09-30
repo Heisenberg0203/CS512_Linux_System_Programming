0767a2b4999d7b4411a07285f4991450f7afd432
Rushikesh <rushikesh0203@gmail.com>
2020-09-30 19:17:43 +0530

    read/write lock implementation

---
+++ 16a.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<unistd.h>
+#include<sys/time.h>
+extern int errno;
+
+int main(int argc, char * argv[]){
+
+  int fd = open("db",O_RDONLY);
+
+
+  if (fd==-1)
+  {
+	perror("Error occured in open\n");
+	printf("%d\n",errno);
+
+  }
+  else{
+
+	  //lock
+	  struct flock lock;
+	  lock.l_type = F_RDLCK;
+	  lock.l_whence = SEEK_SET;
+	  lock.l_start=0;
+	  lock.l_len =0;
+	  lock.l_pid = getpid();
+	  fcntl(fd,F_SETLKW,&lock);
+	  printf("Inside Critical Section with Read Lock\n");
+	  
+	  printf(" Press enter to continue\n ");
+	  getchar();
+	  lock.l_type = F_UNLCK;
+	  fcntl(fd,F_SETLK,&lock);
+	  close(fd);
+	  printf("--FINISH--\n");
+  }
+  return 0;	
+}
