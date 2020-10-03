0b59a76669f44d883580b1445ba78763d6d6b1c8
Rushikesh <rushikesh0203@gmail.com>
2020-10-03 22:11:52 +0530

    lseek, logical space, physical space of a file

--- 10.c
+++ 10.c
@@ -3,11 +3,11 @@
 #include<fcntl.h>
 #include<errno.h>
 extern int errno;
-int main(){
+int main(int arc, char* argv[]){
 
 off_t ret;
 
-int fd = open("10_f.txt",O_RDWR);
+int fd = open(argv[1],O_RDWR);
 
 //go to the end of file
 ret = lseek(fd,0,SEEK_END);
@@ -15,8 +15,8 @@ printf("%ld\n",ret);
 //write 10 bytes
 write(fd,"hellofile ",10);
 ret = lseek(fd,0,SEEK_CUR);
-//move 10 bytes ahead from current position
-ret = lseek(fd,ret+10l,SEEK_SET);
+//move 1000000000 bytes ahead from current position
+ret = lseek(fd,ret+10000000l,SEEK_SET);
 printf("%ld\n",ret);
 // write 10 bytes
 write(fd,"hellofile ",10);
@@ -26,10 +26,12 @@ return 0;
 
 
 /*
- 0000000   T   h   i   s       i   s       t   h   e       b   e   g   i
+0000000   T   h   i   s       i   s       t   h   e       b   e   g   i
 0000020   n   n   i   n   g       o   f       f   i   l   e   .  \n   h
 0000040   e   l   l   o   f   i   l   e      \0  \0  \0  \0  \0  \0  \0
-0000060  \0  \0  \0   h   e   l   l   o   f   i   l   e    
-0000075
+0000060  \0  \0  \0   h   e   l   l   o   f   i   l   e       h   e   l
+0000100   l   o   f   i   l   e      \0  \0  \0  \0  \0  \0  \0  \0  \0
+0000120  \0   h   e   l   l   o   f   i   l   e    
+0000133
 */
-
+// -rw-rw-r-- 1 rushikesh rushikesh 91 Sep 28 03:12 10_f.txt
