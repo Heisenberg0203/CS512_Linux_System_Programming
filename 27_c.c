f4394b2757e420184d6f25c4c5b7a809edfd9d97
Rushikesh <rushikesh0203@gmail.com>
2020-10-03 22:50:07 +0530

    execle

--- 27_c.c
+++ 27_c.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<unistd.h>
-int main(int argc, char **argv,char **envp[]){
+int main(int argc, char **argv){
+  char *envp[] = { "SOURCE=27c.c","TARGET=OUTPUT",NULL};
   execle("/bin/ls","ls","-rl",NULL,envp);    	
   return 0;
 
